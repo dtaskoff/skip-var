2d9ec7c9c4d906c5e59a3be766123cdd0f92717b
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-12-23 18:43:59 +0200

    Synchronise lock changes via a third MVar

--- src/Control/Concurrent/SVar.hs
+++ src/Control/Concurrent/SVar.hs
@@ -22,43 +22,41 @@ module Control.Concurrent.SVar
 
 import Control.Concurrent.MVar
   ( MVar, newEmptyMVar, newMVar
-  , isEmptyMVar, putMVar, takeMVar
+  , putMVar, takeMVar
   )
-import Control.Monad (when)
 
 
 -- | An 'SVar' (skip variable) is a variable which allows for non-blocking updates,
 -- and blocking reads if the stored data has been read already, or if there is no data.
-data SVar a = SVar (MVar a) (MVar ())
+data SVar a = SVar (MVar (a, Maybe (MVar ()))) (MVar ())
 
 -- | Create an empty 'SVar'.
 newEmptySVar :: IO (SVar a)
 newEmptySVar = do
   lock <- newEmptyMVar
-  var <- newMVar undefined
+  var <- newMVar (undefined, Just lock)
   pure (SVar var lock)
 
 -- | Create an 'SVar' which contains the supplied value.
 newSVar :: a -> IO (SVar a)
 newSVar value = do
   lock <- newMVar ()
-  var <- newMVar value
+  var <- newMVar (value, Nothing)
   pure (SVar var lock)
 
 -- | Put a value into an 'SVar'.
 -- Never blocks, always overwrites the current value, if there is one.
 putSVar :: SVar a -> a -> IO ()
-putSVar (SVar var lock) value = do
-  _ <- takeMVar var
-  hasToNotify <- isEmptyMVar lock
-  putMVar var value
-  when hasToNotify $ putMVar lock ()
+putSVar (SVar var _) value = do
+  (_, mlock) <- takeMVar var
+  putMVar var (value, Nothing)
+  mapM_ (\lock -> putMVar lock ()) mlock
 
 -- | Read a value from an 'SVar'.
 -- Blocks if there isn't a new value since the last read, or if the 'SVar' is empty.
 readSVar :: SVar a -> IO a
 readSVar (SVar var lock) = do
   takeMVar lock
-  value <- takeMVar var
-  putMVar var value
+  (value, _) <- takeMVar var
+  putMVar var (value, Just lock)
   pure value
